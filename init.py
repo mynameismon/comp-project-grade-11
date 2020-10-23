7d73c794fca5db436e7cb955ee1872cf729b2802
mynameismon <programminggeekishere@gmail.com>
2020-10-23 10:49:06 +0530

    Fixed errors in init.py

--- init.py
+++ init.py
@@ -11,16 +11,15 @@ def __init__():
     os.mkdir(path)
   except OSError as e:
     pass
-  
-  files = [f for f in os.listdir(path) if os.isfile(os.join(path, f))]
+  os.chdir(path)
+  files = os.listdir()
   files.sort()
   try:
     last_file = files[-1]
-    last_file = last_file[0:-3] + str((int(last_file[-3]) + 1)) + ".py"
+    last_file = last_file[0:-4] + str((int(last_file[-4]) + 1)) + str(".py")
   except IndexError as e:
     last_file = "Approach-No-1.py"
   last_file = path + "\\" + last_file
-  print(last_file)
   file = open(last_file, 'w')
   file.close()
 
