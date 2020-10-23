76e0f7b5430564768db10ee6869dcd1b7f89fe39
sidhant-sriv <sidhant.sriv@gmail.com>
2020-10-23 15:05:14 +0530

    Simple & Compound interest

---
+++ Problem-No-1/Approach-No-1.py
@@ -0,0 +1,21 @@
+#Problem : Write a program to compute simple interest and compound interest given principle, rate and time period.
+
+#Solution :-
+
+p = float(input("Please enter Principal amount:\n"))
+r = float(input("Please enter rate:\n"))
+t = float(input("Please enter time:\n"))
+
+sc = input('Simple interest or compund interest?[s/c]:  ')
+while True:
+    if(sc == 'c' or sc == 'C'):
+        res = p*(1 + r/100)**t
+        print(f"Answer is {res}")
+        break
+    elif(sc=='s' or sc == 'S' ):
+        res = p*r*t/100
+        print(f"Answer is {res}")
+        break
+    else:
+        sc = input("Simple interest or compound interest?[s/c]:  ")
+
