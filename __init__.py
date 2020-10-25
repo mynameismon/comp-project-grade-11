e2d38f0e028b8531c3e5053c298917109d5ceab4
mynameismon <programminggeekishere@gmail.com>
2020-10-25 16:58:18 +0530

    fixed __init__.py for extended support

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from os import getcwd
-import platform, os
+import os
 
 def __init__():
   problemnumber = int(input("Enter the problem number: \n >> "))
@@ -11,15 +11,10 @@ def __init__():
     os.mkdir(path)
   except OSError as e:
     pass
-  os.chdir(path)
-  files = os.listdir()
-  files.sort()
-  try:
-    last_file = files[-1]
-    last_file = last_file[0:-4] + str((int(last_file[-4]) + 1)) + str(".py")
-  except IndexError as e:
-    last_file = "Approach-No-1.py"
-  last_file = path + "\\" + last_file
+  i = 1
+  while os.path.exists("Problem-No-" + str(problemnumber) + "\\Approach-No-%s.py" % i):
+    i+= 1
+  last_file = path + "\\" + "Approach-No-%s.py" % i
   file = open(last_file, 'w')
   file.close()
 
