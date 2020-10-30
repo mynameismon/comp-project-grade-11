87da1394eb105586cdf2a98dda8d26d74fbf9bf5
Bezbakri <67371527+Bezbakri@users.noreply.github.com>
2020-10-30 16:45:44 +0530

    Added part (a) of q28

--- Problem-No-28/Approach-No-1.py
+++ Problem-No-28/Approach-No-1.py
@@ -1,17 +1,12 @@
 '''
-| Ask the user to enter a list of strings. Create a new list that contains only those strings which start with a vowel |
-|----------------------------------------------------------------------------------------------------------------------|
-| Using split(), for-loops and if-statements                                                                           |
+| print pattern:                           |
+| *                                        |
+| **                                       |
+| ***                                      |
+| ****                                     |
+|------------------------------------------|
+| simple for loops and string manipulation |
 '''
 
-n = input("Input a list of strings seperated by commas[,]\n")
-res = ''
-n = n.split(',')
-vowels = 'AEIOUaeiou'
-
-for i in n:
-    if i[0] in vowels:
-        res += i
-print(res)
-
-
+for i in range(1,5):
+    print("*"*i)
