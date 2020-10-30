5a6dbc30c5b4ceef1fd918b8336e648a9b40db4f
Bezbakri <67371527+Bezbakri@users.noreply.github.com>
2020-10-30 17:07:19 +0530

    Added part (a) of q28

---
+++ Problem-No-28/Approach-No-1(a).py
@@ -0,0 +1,12 @@
+'''
+| print pattern:                           |
+| *                                        |
+| **                                       |
+| ***                                      |
+| ****                                     |
+|------------------------------------------|
+| simple for loops and string manipulation |
+'''
+
+for i in range(1,5):
+    print("*"*i)
