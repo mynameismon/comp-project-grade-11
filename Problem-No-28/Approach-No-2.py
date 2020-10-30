55623035054cc8c182944d678418c8f94078abdd
Bezbakri <67371527+Bezbakri@users.noreply.github.com>
2020-10-30 17:09:52 +0530

    Added parts (b) and (c) of q28

---
+++ Problem-No-28/Approach-No-2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 16:47:28 2020
+
+@author: bezbakri
+"""
+
+'''
+| print pattern:                           |
+| *                                        |
+| **                                       |
+| ***                                      |
+| **                                       |
+| *                                        |
+|------------------------------------------|
+| simple for loops and string manipulation |
+'''
+
+
+for i in range(1,4):
+    print("*"*i)
+for i in range(2,0,-1):
+    print("*"*i)
